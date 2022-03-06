8a16376efe0c46203aba9e856455433263757b28
Dhiraj <001greatdsp@gmail.com>
2022-03-06 13:16:12 +0530

    Head,Tail,Describe,Type Function

--- Pr1.py
+++ Pr1.py
@@ -1,5 +1,17 @@
 import pandas as pd
 # Pandas is used to analyze data
-df=pd.read_csv("gov_finance")
+df=pd.read_csv("gov_finance.csv")
 
+print("----Dataset Description-----")
+print(df.describe())
+
+print("----Dataset type-----")
+print(type(df))
+
+print("----Head Data-----")
 print(df.head())
+
+print("----Tail Data-----")
+print(df.tail())
+
+
